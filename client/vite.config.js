845fe3ba483d7042e1de0391899ab31ea57abdc4
magiscurra <akhileshtiss8@gmail.com>
2026-07-05 22:26:29 +0530

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,23 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor-react';
+          }
+          if (id.includes('node_modules/gsap')) {
+            return 'vendor-gsap';
+          }
+          if (id.includes('node_modules/three')) {
+            return 'vendor-three';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
